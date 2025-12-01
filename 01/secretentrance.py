2c75900d7a23ce0f147462b78ce673071e2560c2
jbovee <Mitouch.b@gmail.com>
2025-12-01 12:54:47 -0500

    Day one part two

--- 01/secretentrance.py
+++ 01/secretentrance.py
@@ -9,7 +9,9 @@ from common import get_input
 
 def main():
     inp = get_input(os.path.join(cwd, 'input'))
+    testinp = get_input(os.path.join(cwd, 'test_input'))
     print('Door password is: {}'.format(part_one(inp)))
+    print('Door password is actually: {}'.format(part_two(inp)))
 
 def part_one(inp):
     # starts at 50
@@ -25,6 +27,29 @@ def part_one(inp):
         if pos == 0:
             count += 1
         # print('after| pos: {}'.format(pos))
+    return count
+
+def part_two(inp):
+    # starts at 50
+    # number of times dial points at or passes 0
+    count = 0
+    pos = 50
+    for rot in inp:
+        # print('pos: {}, rot: {}'.format(pos, rot))
+        move = int(rot.replace('L','-').replace('R','+'))
+        if rot[0] == 'L':
+            count += move // -100
+            if pos != 0:
+                count += int((pos + (move % -100)) <= 0)
+            pos = (pos + move) % -100
+            if pos < 0:
+                pos += 100
+        elif rot[0] == 'R':
+            count += move // 100
+            if pos != 0:
+                count += int((pos + (move % 100)) >= 100)
+            pos = (pos + move) % 100
+        # print('after| pos: {}, count: {}'.format(pos, count))
     return count
 
 if __name__ == "__main__":
