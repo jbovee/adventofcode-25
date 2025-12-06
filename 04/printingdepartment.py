1527986eb8612966fef649362b6cfe603e3f74f9
jbovee <Mitouch.b@gmail.com>
2025-12-06 15:00:52 -0500

    Add day four part two

--- 04/printingdepartment.py
+++ 04/printingdepartment.py
@@ -11,6 +11,7 @@ def main() -> None:
     inp = get_input(os.path.join(cwd, 'input'))
     testinp = get_input(os.path.join(cwd, 'test_input'))
     print('Accessible rolls: {}'.format(part_one(inp)))
+    print('Actual accessible rolls: {}'.format(part_two(inp)))
 
 def part_one(inp: [[str]]) -> int:
     accessible = 0
@@ -25,6 +26,30 @@ def part_one(inp: [[str]]) -> int:
                 if neighbors < 4:
                     accessible += 1
     return accessible
+
+def part_two(inp: [[str]]) -> int:
+    totalRemoved = 0
+    warehouse, removed = remove_rolls(inp)
+    totalRemoved += removed
+    while removed > 0:
+        warehouse, removed = remove_rolls(warehouse)
+        totalRemoved += removed
+    return totalRemoved
+
+def remove_rolls(warehouse: [[str]]) -> ([[str]], int):
+    removed = 0
+    for row in range(len(warehouse)):
+        for col in range(len(warehouse[row])):
+            if warehouse[row][col] == '@':
+                neighbors = 0
+                directions = [(row - 1, col), (row - 1, col + 1), (row, col + 1), (row + 1, col + 1), (row + 1, col), (row + 1, col - 1), (row, col - 1), (row - 1, col - 1)]
+                for direction in directions:
+                    if (direction[0] > -1) and (direction[1] > -1) and (direction[0] < len(warehouse)) and (direction[1] < len(warehouse[row])) and warehouse[direction[0]][direction[1]] == '@':
+                        neighbors += 1
+                if neighbors < 4:
+                    removed += 1
+                    warehouse[row][col] = 'X'
+    return (warehouse, removed)
 
 if __name__ == "__main__":
     main()
